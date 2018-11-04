a8b0b1aaf55b975371635a143f6c8eafc6bd5789
Luis Hankel <kaes3kuch3n@gmail.com>
2018-11-04 13:22:34 +0100

    Initial commit

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Mouse : MonoBehaviour {
+
+    [SerializeField]
+    private string buildKey = "Fire1";
+    [SerializeField]
+    private Sprite buildTile;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+        this.transform.position = mousePos;
+
+        ProcessInput(mousePos);
+    }
+
+    private void ProcessInput(Vector2 mousePosition)
+    {
+        if (Input.GetButtonDown(buildKey))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero, 0.0F);
+            
+            if (hit.transform != null)
+            {
+                World.Instance.ChangeTile(hit.transform.gameObject, buildTile);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject startTile;
+    [SerializeField]
+    private int worldWidth;
+    [SerializeField]
+    private int worldHeight;
+
+    private GameObject[,] tiles;
+
+    public static World Instance { get; private set; }
+
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one world script in the scene!");
+        }
+
+        Instance = this;
+
+        tiles = new GameObject[worldWidth, worldHeight];
+
+        for (int y = 0; y < worldWidth; y++)
+        {
+            for (int x = 0; x < worldHeight; x++)
+            {
+                GameObject tile = Instantiate(startTile);
+
+                tile.transform.position = new Vector3(x * 0.5F - y * 0.5F, -(x * 0.25F) - y * 0.25F);
+                tile.GetComponent<SpriteRenderer>().sortingOrder = x + y;
+                tile.transform.parent = this.transform;
+                tile.transform.name = "Tile [" + x + ", " + y + "]";
+                tiles[x, y] = tile;
+            }
+        }
+    }
+
+    public void ChangeTile(GameObject tile, Sprite newTile)
+    {
+        tile.GetComponent<SpriteRenderer>().sprite = newTile;
+    }
+}
